b03b80c6950099505fe14d3f0f3062d0799cfaa9
dhanushns <dhanushns2004@gmail.com>
2024-07-26 20:34:42 +0530

    Strings

--- Strings/subString.java
+++ Strings/subString.java
@@ -1,7 +1,27 @@
 package BLIND_75.Strings;
 
 public class subString {
-    public static void main(String[] args) {
 
+    public static int isSubString(String s,String tar){
+
+        int len = tar.length();
+
+        int i = 0, j = len;
+        while(j <= s.length()){
+            String substr = s.substring(i,j);
+            System.out.println(substr);
+            if(substr.equals(tar))
+                return i;
+            i+=1;
+            j+=1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcabcd";
+        String tar = "abcd";
+        System.out.println(isSubString(s,tar));
     }
 }
